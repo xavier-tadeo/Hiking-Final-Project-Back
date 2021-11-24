2c76bded0e7706c7f79e7c3e52903b6cc26215e2
Tadeo <domenechtadeo@outlook.es>
2021-11-24 23:02:06 +0100

    Added the updateHike in the hikeRoutes

--- server/routes/hikeRoutes.ts
+++ server/routes/hikeRoutes.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import { hikeCreate, hikeDelete, hikeGet } from "../controllers/hikeController";
+import {
+  hikeCreate,
+  hikeDelete,
+  hikeGet,
+  hikeUpdate,
+} from "../controllers/hikeController";
 
 const router = express.Router();
 
 router.post("/create", hikeCreate);
 router.get("/get", hikeGet);
 router.delete("/delete/:hikeId", hikeDelete);
+router.patch("/update/:hikeId", hikeUpdate);
 
 export default router;
