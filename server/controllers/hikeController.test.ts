42f4a1d0d0601b712a790a484dd1fc12443ede17
Tadeo <domenechtadeo@outlook.es>
2021-11-24 18:38:38 +0100

    Create the test for hikeCreate function the method post

--- server/controllers/hikeController.test.ts
+++ server/controllers/hikeController.test.ts
@@ -1,6 +1,6 @@
-import { Response } from "express";
+import { Response, Request } from "express";
 import HikingModel from "../../database/models/hiking";
-import { hikeGet } from "./hikeController";
+import { hikeCreate, hikeGet } from "./hikeController";
 
 const mockResponse = () => {
   const res = {} as Response;
@@ -41,6 +41,61 @@ describe("Given hikeGet function", () => {
       expect(next.mock.calls[0][0]).toHaveProperty(
         "message",
         "Not found anything!"
+      );
+    });
+  });
+});
+
+describe("Given hikeCreate function", () => {
+  describe("When receives a request with new hike", () => {
+    test("Then it should invoke the method json with a new hike", async () => {
+      const reqBody = {
+        title: "Guinardo",
+        description: "Muy buena ruta",
+        map: 543445,
+        images: ["url"],
+        stadistics: 5,
+      };
+      const body: any = reqBody;
+      const req = {
+        body,
+      } as Request;
+      const res = mockResponse();
+      const status = 201;
+      const next = jest.fn();
+
+      HikingModel.create = jest.fn().mockResolvedValue(reqBody);
+
+      await hikeCreate(req, res, next);
+
+      expect(res.json).toHaveBeenCalledWith(reqBody);
+      expect(res.status).toHaveBeenCalledWith(status);
+    });
+  });
+  describe("When reject promese", () => {
+    test("It should invoke the next function with error, code 400 and message 'Bad create routes'", async () => {
+      const requestBody = {
+        tattooStyles: "realista",
+        image: "url",
+      };
+
+      const req = {
+        body: requestBody,
+      } as Request;
+
+      const res = mockResponse();
+      const error = new Error();
+      const next = jest.fn();
+
+      HikingModel.create = jest.fn().mockRejectedValue(error);
+
+      await hikeCreate(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(error);
+      expect(next.mock.calls[0][0]).toHaveProperty("code", 400);
+      expect(next.mock.calls[0][0]).toHaveProperty(
+        "message",
+        "Bad create routes"
       );
     });
   });
