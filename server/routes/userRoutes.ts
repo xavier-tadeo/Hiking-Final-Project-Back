9404889efe074c88e0f32f62e1b920e07e32b254
Tadeo <domenechtadeo@outlook.es>
2021-11-24 09:48:27 +0100

    Create the routes for method delete user

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,10 +1,15 @@
 import express from "express";
 
-import { userCreate, userLogin } from "../controllers/userController";
+import {
+  userCreate,
+  userDelete,
+  userLogin,
+} from "../controllers/userController";
 
 const router = express.Router();
 
 router.post("/register", userCreate);
 router.post("/login", userLogin);
+router.delete("/delete/:idUser", userDelete);
 
 export default router;
