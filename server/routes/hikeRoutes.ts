21103d0033607ce6bff648b8a6547e366bd4322a
Tadeo <domenechtadeo@outlook.es>
2021-11-25 09:36:57 +0100

    Added in the hikeRoutes method get for one hike

--- server/routes/hikeRoutes.ts
+++ server/routes/hikeRoutes.ts
@@ -3,6 +3,7 @@ import {
   hikeCreate,
   hikeDelete,
   hikeGet,
+  hikeGetOne,
   hikeUpdate,
 } from "../controllers/hikeController";
 
@@ -12,5 +13,6 @@ router.post("/create", hikeCreate);
 router.get("/get", hikeGet);
 router.delete("/delete/:hikeId", hikeDelete);
 router.patch("/update/:hikeId", hikeUpdate);
+router.get("/get/:hikeId", hikeGetOne);
 
 export default router;
