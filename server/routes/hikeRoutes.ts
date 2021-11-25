d3080b8bcb8502cee215abb82ac1279d40a22f2e
Tadeo <domenechtadeo@outlook.es>
2021-11-25 18:09:24 +0100

    Added the auth in the routes hike ans user need

--- server/routes/hikeRoutes.ts
+++ server/routes/hikeRoutes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+
 import {
   hikeCreate,
   hikeDelete,
@@ -8,13 +9,14 @@ import {
 } from "../controllers/hikeController";
 import firebase from "../middlewares/firebase";
 import upload from "../middlewares/upload";
+import auth from "../middlewares/auth";
 
 const router = express.Router();
 
-router.post("/create", upload.single("images"), firebase, hikeCreate);
+router.post("/create", auth, upload.single("images"), firebase, hikeCreate);
 router.get("/get", hikeGet);
-router.delete("/delete/:hikeId", hikeDelete);
-router.patch("/update/:hikeId", hikeUpdate);
+router.delete("/delete/:hikeId", auth, hikeDelete);
+router.patch("/update/:hikeId", auth, hikeUpdate);
 router.get("/get/:hikeId", hikeGetOne);
 
 export default router;
