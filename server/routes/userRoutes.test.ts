e27a5d5382c2b3285efaffcc5b1fb532fb4159f1
Tadeo <domenechtadeo@outlook.es>
2021-11-26 23:41:52 +0100

    Create the supertest for enpoint user/register

--- server/routes/userRoutes.test.ts
+++ server/routes/userRoutes.test.ts
@@ -49,7 +49,7 @@ describe("Given a route /user/login", () => {
     test("Then it should respond with a response and code 200", async () => {
       await request
         .post("/user/login")
-        .send({ name: "Sonia", password: "Sonia", email: "arlet@arlet.com" })
+        .send({ name: "Sonia", password: "Sonia" })
         .expect(200);
     });
   });
@@ -58,7 +58,26 @@ describe("Given a route /user/login", () => {
     test("Then it should respond with a response with 'Endpoint not found 404'", async () => {
       await request
         .post("/user/log")
+        .send({ name: "Sonia", password: "Sonia" })
+        .expect(404);
+    });
+  });
+});
+
+describe("Given a route user/register", () => {
+  describe("When a POST request arrives good endpoint", () => {
+    test("Then is should respond with a respose and code 200", async () => {
+      await request
+        .post("/user/register")
         .send({ name: "Sonia", password: "Sonia", email: "arlet@arlet.com" })
+        .expect(200);
+    });
+  });
+  describe("When a POST request arrives a wrong endpoint", () => {
+    test("Then it should respond with a response with 'Endpoint not found 404'", async () => {
+      await request
+        .post("/user/regi")
+        .send({ name: "Sonia", password: "Sonia" })
         .expect(404);
     });
   });
